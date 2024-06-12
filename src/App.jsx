8f4103810acdc6b4646ba41253321a60b0e91ed5
Abhishek singh <abhiramgarhia13@gmail.com>
2024-06-12 17:19:56 -0400

    done

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css"; // Import the CSS file for styling
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const apiKey = "5327fbf2d35ff94285fee76b42e4cc05";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+
+  const searchPlace = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url)
+        .then((response) => {
+          setData(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching the weather data:", error);
+        });
+      setLocation("");
+    }
+  };
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchPlace}
+          placeholder="Enter the city"
+          type="text"
+        />
+      </div>
+      <div className="container">
+        {data.main && (
+          <>
+            <div className="top">
+              <div className="location">
+                <p>{data.name}</p>
+              </div>
+              <div className="temp">
+                <h1>{Math.round(data.main.temp)}°C</h1>
+              </div>
+              <div className="description">
+                <div className="icon">
+                  <img
+                    src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                    alt={data.weather[0].description}
+                  />
+                </div>
+                <p className="shift">{data.weather[0].description}</p>
+              </div>
+            </div>
+            <div className="bottom">
+              <div className="feels">
+                <p className="bold">{Math.round(data.main.feels_like)}°C</p>
+                <p>Feels like</p>
+              </div>
+              <div className="humidity">
+                <p className="bold">{data.main.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                <p className="bold">{Math.round(data.wind.speed)} MPH</p>
+                <p>Wind</p>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
